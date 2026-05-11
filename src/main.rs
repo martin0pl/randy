219c24a3838bd9a35d673f80d92de2691636b9ef
Martin <168760158+martin0pl@users.noreply.github.com>
2026-05-11 22:14:41 +0200

    Base of the script with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(name = "randy")]
+#[command(about = "CLI tool to generate random things anywhere on your device", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Launch a dice
+    Dice {
+        /// Number of faces of the dice
+        nb_faces: i32,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Dice { nb_faces } => {
+            println!("Launch a dice of {} faces", nb_faces);
+        }
+    }
 }
